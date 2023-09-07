cf13abda7f5e88d6a5bd6c0c4018ce9b1d05f0d7
Jesse Bofill <jesse_bofill@yahoo.com>
2023-09-07 14:17:10 -0600

    change how search bar patch finds element and add logging if it fails to do so

--- src/searchBarPatch.tsx
+++ src/searchBarPatch.tsx
@@ -2,20 +2,35 @@ import { findInReactTree, sleep } from "decky-frontend-lib"
 import { tabManager } from "./classes/TabManager"
 import { SearchBarInput } from "./components/SearchBarInput"
 import { reactTree, routePath } from "./init"
+import { logN } from './log'
 
-export let unpatchSearchBar: () => void
+export let unpatchSearchBar: () => void = () => {}
 export const patchSearchBar = async () => {
     let searchBarRootNode: any
+    let t = 0
     while (!searchBarRootNode) {
         searchBarRootNode = findInReactTree(reactTree, node => node?.type?.toString().includes('SetUniversalSearchFocused') && node?.type?.toString().includes('GetForceHeaderAfterResume'))
+        if (searchBarRootNode) continue
+        if (t >= 20000) {
+            logN('Search Bar Patch', `Failed to find search bar root node after ${t / 1000}`)
+            return
+        }
+        t += 200
         await sleep(200)
     }
     const orig = searchBarRootNode.type
     const searchBarRootWrapper = (props: any) => {
         const ret = orig(props)
         if (window.location.pathname === '/routes' + routePath) {
-            const parent = ret.props.children.props?.children?.[1] ? ret.props.children : ret
-            parent.props.children[1] = <SearchBarInput tabManager={tabManager} />
+            const res = findLocation(ret, node => {
+                if (!node.type) return false
+                const fnString = node.type.toString?.()
+                return fnString?.includes('GamepadUI.Search.Root') && fnString?.includes('SearchBox')
+            })
+            if (res) {
+                const [parent, key] = res
+                parent[key] = <SearchBarInput tabManager={tabManager} />
+            } else logN("Search Bar Patch", 'Failed to find search bar element to patch')
         }
         return ret
     }
@@ -29,3 +44,30 @@ export const patchSearchBar = async () => {
         searchBarRootNode.alternate.type = searchBarRootNode.type;
     }
 }
+
+const findLocation = (parent, filter) => {
+    const walkable = ['props', 'children']
+    if (!parent || typeof parent !== 'object') return
+    if (Array.isArray(parent)) {
+        for (let i = 0; i < parent.length; i++) {
+            if (!parent[i]) continue
+            if (filter(parent[i])) return [parent, i]
+        }
+        for (let i = 0; i < parent.length; i++) {
+            if (!parent[i]) continue
+            const next = findLocation(parent[i], filter)
+            if (next) return next
+        }
+    } else {
+        for (const prop of walkable) {
+            if (!parent[prop]) continue
+            if (filter(parent[prop])) return [parent, prop]
+        }
+
+        for (const prop of walkable) {
+            if (!parent[prop]) continue
+            const next = findLocation(parent[prop], filter)
+            if (next) return next
+        }
+    }
+} 
