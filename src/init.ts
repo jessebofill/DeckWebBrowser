d85932894834de6599068eaca628a24424fcc540
Jesse Bofill <jesse_bofill@yahoo.com>
2023-12-16 13:34:12 -0700

    switch to global dfl and fix issue on latest Steam Client Beta

--- src/init.ts
+++ src/init.ts
@@ -1,4 +1,4 @@
-import { Router } from "decky-frontend-lib";
+import { Router, getReactRoot } from "decky-frontend-lib";
 // import { name } from '../plugin.json'
 
 export const pluginName = 'Web Browser'
@@ -7,6 +7,6 @@ export const defaultUrl = 'https://store.steampowered.com'
 
 export const windowRouter = Router.WindowStore?.GamepadUIMainWindowInstance
 export const SP_Window = windowRouter?.BrowserWindow
-export const reactTree = (document.getElementById('root') as any)._reactRootContainer._internalRoot.current;
+export const reactTree = getReactRoot(document.getElementById('root') as any)
 
 
