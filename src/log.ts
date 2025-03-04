faf801a1e068afc00e5088274ae5dbb196161c86
Jesse Bofill <jesse_bofill@yahoo.com>
2025-03-03 21:20:20 -0700

    chore: update logger

--- src/log.ts
+++ src/log.ts
@@ -9,6 +9,9 @@ export const warn = (...args) => {
 export const error = (...args) => {
     console.error(`%c ${pluginName} %c`, 'background: #7627D3; color: #E5E1EA;', 'background: transparent;', ...args);
 };
+export const debug = (...args) => {
+    console.debug(`%c ${pluginName} %c`, 'background: #7627D3; color: #E5E1EA;', 'background: transparent;', ...args);
+};
 
 export const logN = (name, ...args) => {
     console.log(`%c ${pluginName} %c ${name} %c`, 'background: #7627D3; color: #E5E1EA;', 'background: #AF70F9; color: black;', 'background: transparent;', ...args);
@@ -18,4 +21,34 @@ export const warnN = (name, ...args) => {
 }
 export const errorN = (name, ...args) => {
     console.error(`%c ${pluginName} %c ${name} %c`, 'background: #7627D3; color: #E5E1EA;', 'background: #AF70F9; color: black;', 'background: transparent;', ...args);
+}
+export const debugN = (name, ...args) => {
+    console.debug(`%c ${pluginName} %c ${name} %c`, 'background: #7627D3; color: #E5E1EA;', 'background: #AF70F9; color: black;', 'background: transparent;', ...args);
+}
+export const throwN = (name, ...args: any[]) => {
+    throw new Error([`%c ${pluginName} %c ${name} %c`, 'background: #7627D3; color: #E5E1EA;', 'background: #AF70F9; color: black;', 'background: transparent;', ...args].join(' '));
+}
+
+export class Logger {
+    constructor(private name: string) {
+        this.name = name;
+    }
+    log(...args: any[]) {
+        logN(this.name, ...args);
+    }
+
+    debug(...args: any[]) {
+        debugN(this.name, ...args);
+    }
+
+    warn(...args: any[]) {
+        warnN(this.name, ...args);
+    }
+
+    error(...args: any[]) {
+        errorN(this.name, ...args);
+    }
+    throw(...args: any[]) {
+        throwN(this.name, ...args);
+    }
 }
